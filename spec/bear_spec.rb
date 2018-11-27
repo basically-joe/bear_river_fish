cf58537e7e57a3a44d8c5ac17b54e784c9d33300
Joe McColl <joe4kool@gmail.com>
2018-11-27 21:18:23 +0000

    giving bear access to river and fish data

--- spec/bear_spec.rb
+++ spec/bear_spec.rb
@@ -1,12 +1,23 @@
 require("minitest/autorun")
 require("minitest/rg")
 require_relative("../bear")
+require_relative("../river")
+require_relative("../fish")
 
 
 class BearTest < MiniTest::Test
 
   def setup
     @bear1 = Bear.new("Yogi", "Grizzly")
+
+    @river = River.new("Amazon", ["Nemo", "Flounder", "Dory", "Wanda", "Jaws"])
+
+    @fish_nemo = Fish.new("Nemo")
+    @fish_flounder = Fish.new("Flounder")
+    @fish_dory = Fish.new("Dory")
+    @fish_wanda = Fish.new("Wanda")
+    @fish_jaws = Fish.new("Jaws")
+
   end
 
 def test_bear_name
