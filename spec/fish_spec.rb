e5ca2c9e81c1326a536d08a703534eed68641b84
Joe McColl <joe4kool@gmail.com>
2018-11-27 20:54:13 +0000

    created fish spec and initial fail for testing fish name

---
+++ spec/fish_spec.rb
@@ -0,0 +1,20 @@
+require("minitest/autorun")
+require("minitest/rg")
+require_relative("../fish")
+
+
+class FishTest < MiniTest::Test
+
+  def setup
+    @fish1 = Fish.new("Nemo")
+    @fish2 = Fish.new("Flounder")
+    @fish3 = Fish.new("Dory")
+    @fish4 = Fish.new("Wanda")
+    @fish5 = Fish.new("Jaws")
+  end
+
+  def test_fish_name
+    assert_equal("Nemo", @fish.name)
+  end
+
+end
